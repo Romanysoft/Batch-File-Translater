089d6f393d0038ec90b28c21e085d784abb41b54
iansun <lauer3912@gmail.com>
2016-04-20 10:12:18 +0800

    网站提交

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"Batch File Translater",
+        appID:"com.romanysoft.app.macos.FileTranslater",
+        documentTitle:"BatchFileTranslater By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-43238484-11",
+        gitHome:"//github.com/Romanysoft/Batch-File-Translater",
+        reportIssueUrl:"//github.com/Romanysoft/Batch-File-Translater/issues",
+        changeLogUrl:"//github.com/Romanysoft/Batch-File-Translater/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/Batch-File-Translater/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
